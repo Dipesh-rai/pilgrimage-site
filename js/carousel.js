bf6188feeb18305871a1e4728fdad9c5b23e5db6
Dipesh-rai <rdipesh48@gmil.com>
2025-05-12 18:02:12 +0545

    update

---
+++ js/carousel.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".testimonial_part").owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 10,
+        lazyLoad: true,
+        nav:true,
+        dots:true,
+        responsive: {
+            0: {
+                items: 1 // for mobile
+            },
+            768: {
+                items: 2 // for tablets
+            },
+            1250: {
+                items: 3 // for desktops
+            }
+        }
+    }
+
+
+    );
+});
